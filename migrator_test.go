5181f9694d0d521994e288950ab9f35d81725a74
Camilo Aguilar <camilo.aguilar@gmail.com>
2015-03-04 18:09:24 -0500

    Adds license

--- migrator_test.go
+++ migrator_test.go
@@ -1,2 +1,6 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, version 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 //go:generate go-bindata -nomemcopy -pkg migrations -o migrations/migrations.go migrations/...
 package migrator
