638c6c3112fe3343bd750ad3e7fb51b6990aa048
Camilo Aguilar <camilo.aguilar@gmail.com>
2015-03-23 18:13:10 -0400

    Work in progress.

---
+++ cmd/migrator.go
@@ -0,0 +1,13 @@
+package main
+
+// migrator migrate
+// migrator rollback -step 3
+// migrator rollback
+// migrator redo
+// migrator redo -step 3
+// migrator up -version 0003
+// migrator down -version 0003
+// reads DB URL from environment variable
+func main() {
+
+}
